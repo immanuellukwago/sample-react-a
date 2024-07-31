fd8b7cc5a5ac32aa2b0b86fa6b811d624703045c
immanuellukwago <immanuellukwago@gmail.com>
2024-07-31 15:42:38 +0300

    created the footers and headers

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+   const date = <Date></Date>;
+   const name = "ImmanuelCode";
+
+   return (
+      <footer>
+         <p>
+            @ {date} {name}
+         </p>
+      </footer>
+   );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+function Header() {
+   return (
+      <header>
+         <h1>ImmanuelCode</h1>
+         <ul>
+            <li>
+               <a href="#">Home</a>
+            </li>
+            <li>
+               <a href="#">About</a>
+            </li>
+            <li>
+               <a href="#">Languages & Frameworks</a>
+            </li>
+            <li>
+               <a href="#">Projects</a>
+            </li>
+         </ul>
+      </header>
+   );
+}
+
+export default Header;
